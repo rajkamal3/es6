3b9dd1e92ee623a5c93b744de442b464ed601a98
Raj Kamal Chenumalla <rkchenumalla@gmail.com>
2020-10-16 00:57:05 +0530

    wrapping functions

--- src/proxyApi.js
+++ src/proxyApi.js
@@ -2,16 +2,42 @@ console.clear();
 
 // Traps
 
-let person = {
-	age: 24
-};
+// let person = {
+// 	age: 24,
+// 	name: 'Max'
+// };
+
+// let handler = {
+// 	get: function (target, name) {
+// 		return name in target ? target[name] : 'Non existent';
+// 	},
+// 	set: function (target, property, value) {
+// 		if (value.length >= 2) {
+// 			Reflect.set(target, property, value);
+// 		}
+// 	}
+// };
+
+// var proxy = new Proxy(person, handler);
+
+// proxy.name = 'huell';
+
+// console.log(proxy.name);
+
+// Wrapping functions
+
+function log(message) {
+	console.log('Log entry created, message:' + message);
+}
 
 let handler = {
-	get: function (target, name) {
-		return name in target ? target[name] : 'Non existent';
+	apply: function (target, thisArg, argList) {
+		if (argList.length === 1) {
+			return Reflect.apply(target, thisArg, argList);
+		}
 	}
 };
 
-var proxy = new Proxy(person, handler);
+let proxy = new Proxy(log, handler);
 
-console.log(proxy.name);
+proxy('hello', 'huell');
